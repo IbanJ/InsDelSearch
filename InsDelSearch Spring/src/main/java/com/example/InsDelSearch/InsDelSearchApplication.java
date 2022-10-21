22de454af9fc2937e385fc6f769c966944e6f6e3
IbanJ <iban6698@hotmail.com>
2022-10-21 12:38:10 +0200

    first commit

---
+++ InsDelSearch Spring/src/main/java/com/example/InsDelSearch/InsDelSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.InsDelSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsDelSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsDelSearchApplication.class, args);
+	}
+
+}
---
+++ InsDelSearch Spring/src/test/java/com/example/InsDelSearch/InsDelSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InsDelSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsDelSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
